8b8d09c260bb01614facc355e9a039fa65d777c0
sethripman <sethripman@gmail.com>
2020-01-28 15:31:31 -0800

    updated with recent starter code

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './App.css';
 
-const Header = () => (
-  <header className={styles.Header}>
-    <nav>
-      <ul>
-        <li><a className={styles.active} href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Documentation</a></li>
-      </ul>
-    </nav>
-    <section>
-      <h1>The Rick And Morty Api</h1>
-      <h2>Hey, did you ever want to hold a Terry fold?</h2>
-    </section>
-  </header>
-);
+class Header extends Component {
+  render() {
+    return (
+      <header className={styles.Header}>
+        <nav>
+          <ul>
+            <li><a className={styles.active} href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Documentation</a></li>
+          </ul>
+        </nav>
+        <section>
+          <h1>The Rick And Morty Api</h1>
+          <h2>Hey, did you ever want to hold a Terry fold?</h2>
+        </section>
+      </header>
+    );
+  }
+};
 
-const Footer = () => (
-  <footer className={styles.Footer}>
-    <section className={styles.stats}>
-      <p>Characters: 493</p>
-      <p>Locations: 76</p>
-      <p>Episodes: 31</p>
-    </section>
-    <section className={styles.acknowledgment}>
-      <p>❮❯ by <a href="#">Axel Fuhrmann</a> 2020</p>
-    </section>
-  </footer>
-);
+class Footer extends Component {
+  render() {
+    return (
+      <footer className={styles.Footer}>
+        <section className={styles.stats}>
+          <p>Characters: 493</p>
+          <p>Locations: 76</p>
+          <p>Episodes: 31</p>
+        </section>
+        <section className={styles.acknowledgment}>
+          <p>❮❯ by <a href="#">Axel Fuhrmann</a> 2020</p>
+        </section>
+      </footer>
+    );
+  }
+}
 
-const App = () => (
-  <React.Fragment>
-    <Header />
-    <Footer />
-  </React.Fragment>
-);
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Footer />
+      </React.Fragment>
+    );
+  }
+};
 
 export default App;
