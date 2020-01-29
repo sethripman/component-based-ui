5ddea82d8ca76831c6be9864040e00f4a4bd8e34
sethripman <sethripman@gmail.com>
2020-01-28 16:16:25 -0800

    starting character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,2 +1,15 @@
 import React, { Component } from 'react';
+import styles from './App.css';
 
+export default class Character extends Component {
+  render() {
+    const { title, description } = this.props;
+
+    return (
+      <>
+        <dt>{title}</dt>
+        <dd>{description}</dd>
+      </>
+    )
+  }
+}
